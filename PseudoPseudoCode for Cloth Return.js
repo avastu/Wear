e6f33ed70dd7e4a4929e47d60649f05a6bdb699f
Utsav Ahuja <utsavahu@usc.edu>
2014-07-10 16:59:41 -0700

    Half Pseudo half implementable code for suggestions

---
+++ PseudoPseudoCode for Cloth Return.js
@@ -0,0 +1,59 @@
+// JavaScript source code
+//1 = cool
+//2 = warm
+//3 = average
+temp = undefined;
+indic = getUserChoice.indic;
+if (indic === 1) {
+    temp = low;
+    getRec(temp);
+}
+else if (indic === 2) {
+    temp = high;
+    getRec(temp);
+}
+else if(indic === 3) {
+    temp = (high + low)/2;
+    getRec(temp);
+    //where getRec is the method to get the recommendation
+}
+
+function whatToWear(temp) {
+    if(temp < 45) {
+        if(rain()) {
+
+        }
+    }
+    else if(temp < 55) {
+        if(rain()) {
+
+        }
+    }
+    else if(temp < 65) {
+        if(rain()) {
+
+        }
+    }
+    else if(temp < 75) {
+        if(rain()) {
+
+        }
+    }
+    else if(temp < 85) {
+        if(rain()) {
+
+        }
+    }
+    else if(temp < 95) {
+        if(rain()) {
+
+        }
+    }
+} 
+    
+function rain(chprep) { //where chprep is chance of precipitation
+    if(chprep >= 40)
+        return true;
+    return false;
+}
+
