7354056cb877535f10c2d83224af0839198a528a
Matthew Chun <tennismc96@gmail.com>
2014-07-10 16:22:45 -0700

    added VS project

---
+++ Wear/Wear/routes/weather.js
@@ -0,0 +1,8 @@
+﻿exports.list = function (req, res) {
+    //respond to out request with a simple answer
+    res.send({
+        message : "Successfully called station api"
+    });
+
+    return;
+}
