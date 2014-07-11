457487fa4614a59158581af0092ba59f04fb5767
Anna Liu <yuhuiliu1@gmail.com>
2014-07-10 19:02:58 -0700

    Adding angular files

---
+++ js/wear.js
@@ -0,0 +1,25 @@
+﻿var wear = angular.module("wear", ['ngRoute']);
+
+wear.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/today', {
+            /*templateUrl: 'views/stations.html',
+            controller: 'StationsCtrl',*/
+            activetab: 'today'
+        }).
+        when('/tomorrow', {
+            /*templateUrl: 'views/trains.html',
+            controller: 'TrainsCtrl',*/
+            activetab: 'trains'
+        }).
+        otherwise({
+            redirectTo: '/today'
+        });
+  }]);
+
+//Create the MainCtrl Controller...
+wear.controller("MainCtrl", ['$scope', '$http', '$route', function ($scope, $http, $route) {
+    $scope.$route = $route;
+}]);
+
